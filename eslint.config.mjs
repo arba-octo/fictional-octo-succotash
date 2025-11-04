0644762aff53cbce7c78763e489153f8f304cae8
arba-octo <121129925+arba-octo@users.noreply.github.com>
2025-11-04 13:29:22 +0300

    octo-4 feat: рефакторинг CookiesProvider (#4)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,31 +8,39 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 
   {
-    'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
-    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'no-trailing-spaces': 'error',
-    'eol-last': ['error', 'always'],
-    'object-curly-spacing': ['error', 'always'],
-    curly: ['error', 'all'],
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-      { blankLine: 'any',    prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
-      { blankLine: 'always', prev: '*', next: 'return' }
-    ],
-    'arrow-body-style': ['error', 'as-needed'],
-    'no-console': ['error', { allow: ['warn', 'info', 'error'] }]
-  }
+    name: 'yarmy',
+
+    settings: {
+      react: { version: 'detect' },
+    },
+
+    rules: {
+      'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
+      indent: ['error', 2, { SwitchCase: 1 }],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
+      'object-curly-spacing': ['error', 'always'],
+      curly: ['error', 'all'],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+        { blankLine: 'always', prev: '*', next: 'return' },
+      ],
+      'arrow-body-style': ['error', 'as-needed'],
+      'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
+    }
+  },
 ]);
 
 export default eslintConfig;
