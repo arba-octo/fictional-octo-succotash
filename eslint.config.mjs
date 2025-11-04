fdccce4e5ef793349792233ecf5ce0b916877e2f
Arbachakov Valerii <v.arbachakov.v@gmail.com>
2025-11-04 13:07:42 +0300

    octo-3 feat: добавлена авторизация через соц. сети

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,26 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+
+  {
+    'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'eol-last': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
+    curly: ['error', 'all'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any',    prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      { blankLine: 'always', prev: '*', next: 'return' }
+    ],
+    'arrow-body-style': ['error', 'as-needed'],
+    'no-console': ['error', { allow: ['warn', 'info', 'error'] }]
+  }
 ]);
 
 export default eslintConfig;
